ea23c14ee45618bed67142f87f2be0c981808ea5
loris.martini <loris.martini@buonarroti.tn.it>
2023-09-21 16:19:39 +0200

    feat: inizializzato menù nel main }

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        boolean flag;
+        int scelta = -1;
 
+        do{
+            System.out.println("\n1) Inserire i dati. \n2) Volume. \n3)Superficie \n0) Esci");
+            scelta = input.nextInt();
+        }while(scelta != 0);
     }
 }
